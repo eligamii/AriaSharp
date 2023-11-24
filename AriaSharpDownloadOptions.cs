23810819c9508810eeeb01732085770e9b06e8f4
eligami <68642361+eligamii@users.noreply.github.com>
2023-11-24 20:19:26 +0100

    Initial commit

---
+++ AriaSharpDownloadOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AriaSharp
+{
+    public class AriaSharpDownloadOptions // WIP
+    {
+        /// <summary>
+        /// The directory to store the downloaded file.
+        /// </summary>
+        public string Dir { get; set; }
+        /// <summary>
+        /// The file name of the downloaded file. It is always relative to the directory given in Dir option.
+        /// </summary>
+        public string Out { get; set; }
+    }
+}
